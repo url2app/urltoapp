a9d2140554866f43d4ef258f46790438c156f788
douxxtech <douxx@douxx.tech>
2025-03-03 18:28:45 +0100

    Added prevention of being run as admin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-ver = process.versions.node;
+const ver = process.versions.node;
 
 if (ver < '22.0.0') {
   console.error("You need a nodejs installation equal or superior to 22.0.0. Please upgrade your node installation and retry.");
   process.exit(1);
 }
-
 
 const { program } = require('commander');
 const { createApp } = require('./commands/create');
@@ -14,45 +13,50 @@ const { listApps } = require('./commands/list');
 const { removeApp } = require('./commands/remove');
 const { version } = require('../package.json');
 const { setupConfig } = require('./utils/config');
+const { checkNotRoot } = require('./utils/noroot');
 
-setupConfig();
+(async () => {
+  await checkNotRoot();
 
-program
-  .name('u2a')
-  .description('Convert websites into desktop applications')
-  .version(version);
+  setupConfig();
 
   program
-  .command('create <url>')
-  .description('Create a new application from a URL')
-  .option('--name <name>', 'Specify the application name')
-  .option('--width <width>', 'Specify the window width', parseInt)
-  .option('--height <height>', 'Specify the window height', parseInt)
-  .option('--executable [windows|darwin|linux]', 'Create a single executable for the target system')
-  .option('--arch [x64|armv7l|arm64|universal]', 'Specify the target architecture for the executable')
-  .option('--setup', 'Creates a setup file for the executable')
-  .action((url, options) => {
-    createApp(url, options);
+    .name('u2a')
+    .description('Convert websites into desktop applications')
+    .version(version);
+
+  program
+    .command('create <url>')
+    .description('Create a new application from a URL')
+    .option('--name <name>', 'Specify the application name')
+    .option('--width <width>', 'Specify the window width', parseInt)
+    .option('--height <height>', 'Specify the window height', parseInt)
+    .option('--executable [windows|darwin|linux]', 'Create a single executable for the target system')
+    .option('--arch [x64|armv7l|arm64|universal]', 'Specify the target architecture for the executable')
+    .option('--setup', 'Creates a setup file for the executable')
+    .action((url, options) => {
+      createApp(url, options);
+    });
+
+  program
+    .command('list')
+    .description('List all available applications')
+    .action(listApps);
+
+  program
+    .command('remove <url>')
+    .description('Remove an existing application')
+    .action(removeApp);
+
+  program.on('command:*', () => {
+    console.error(`\nInvalid command: ${program.args.join(' ')}`);
+    console.log(`\nUse --help to see the list of available commands.`);
+    process.exit(1);
   });
 
-program
-  .command('list')
-  .description('List all available applications')
-  .action(listApps);
+  program.parse(process.argv);
 
-program
-  .command('remove <url>')
-  .description('Remove an existing application')
-  .action(removeApp);
-
-program.on('command:*', () => {
-  console.error(`\nInvalid command: ${program.args.join(' ')}`);
-  console.log(`\nUse --help to see the list of available commands.`);
-  process.exit(1);
-});
-
-program.parse(process.argv);
-
-if (process.argv.length <= 2) {
-  program.help();
-}
+  if (process.argv.length <= 2) {
+    program.help();
+  }
+})();
---
+++ src/utils/noroot.js
@@ -0,0 +1,32 @@
+const os = require('os');
+const isAdmin = require('is-admin');
+const Logger = require('./logger')
+
+const logger = new Logger('noroot');
+
+async function checkNotRoot() {
+  const platform = os.platform();
+
+  switch (platform) {
+    case 'win32':
+      if (await isAdmin()) {
+        logger.error('This application should not be run as an administrator.', '');
+        process.exit(1);
+      }
+      break;
+
+    case 'darwin':
+    case 'linux':
+      if (process.getuid() === 0) {
+        logger.error('This application should not be run as root.', '');
+        process.exit(1);
+      }
+      break;
+
+    default:
+      logger.error('Unsupported platform:', platform);
+      process.exit(1);
+  }
+}
+
+module.exports = { checkNotRoot };
