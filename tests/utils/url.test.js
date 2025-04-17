068e8f2c1cbfc0ef94b85b776ba9ff67132434bc
douxxtech <douxx@douxx.tech>
2025-04-17 16:00:49 +0200

    vitest to jest

--- tests/utils/url.test.js
+++ tests/utils/url.test.js
@@ -1,16 +1,16 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest';
 const { normalizeUrl, getDomainName } = require('../../src/utils/url');
 const axios = require('axios');
 
+jest.mock('axios');
 
 describe('normalizeUrl', () => {
   beforeEach(() => {
-    vi.clearAllMocks();
+    jest.clearAllMocks();
   });
 
   it('should return the same url if starts with https://', async () => {
     const url = 'https://example.com';
-    const spy = vi.spyOn(axios, 'get').mockResolvedValueOnce({});
+    const spy = jest.spyOn(axios, 'get').mockResolvedValueOnce({});
 
     const result = await normalizeUrl(url);
 
@@ -19,7 +19,7 @@ describe('normalizeUrl', () => {
   });
 
   it('should add https:// and do a test request', async () => {
-    const spy = vi.spyOn(axios, 'get').mockResolvedValueOnce({});
+    const spy = jest.spyOn(axios, 'get').mockResolvedValueOnce({});
 
     const result = await normalizeUrl('example.com');
 
@@ -27,7 +27,7 @@ describe('normalizeUrl', () => {
     expect(spy).toHaveBeenCalledWith('https://example.com');
   });
 
-  it('should add http:// if the requests fails', async () => {
+  it('should add http:// if the request fails', async () => {
     axios.get.mockRejectedValueOnce(new Error('HTTPS failed'));
 
     const result = await normalizeUrl('example.com');
@@ -38,15 +38,15 @@ describe('normalizeUrl', () => {
 });
 
 describe('getDomainName', () => {
-  it('should returns example.com if given https://example.com/helloworld', () => {
+  it('should return example.com if given https://example.com/helloworld', () => {
     expect(getDomainName('https://example.com/helloworld')).toBe('example.com');
   });
 
-  it('should returns sub.example.com if given https://sub.example.com/helloworld', () => {
+  it('should return sub.example.com if given https://sub.example.com/helloworld', () => {
     expect(getDomainName('https://sub.example.com/helloworld')).toBe('sub.example.com');
   });
 
-  it('should returns example.com if given https://www.example.com/helloworld', () => {
+  it('should return example.com if given https://www.example.com/helloworld', () => {
     expect(getDomainName('https://www.example.com/helloworld')).toBe('example.com');
   });
 });
